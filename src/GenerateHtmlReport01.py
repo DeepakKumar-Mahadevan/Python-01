0bd44c783921b0ee27352b0516dfc1669012a26e
Deepak Kumar Mahadevan <connectdeepakkumar.m@gmail.com>
2022-12-29 22:22:19 +0530

    Added new python programs to generate HTML reports

---
+++ src/GenerateHtmlReport01.py
@@ -0,0 +1,28 @@
+import pandas as pd
+# df = pd.read_csv(r'E:\Chicago_E_Drive\Deepak DOCs\Study\Data Analytics\Sample_Data\ManUtdPlayers.csv',encoding='latin-1')
+df = pd.read_excel(r'E:\Chicago_E_Drive\Deepak DOCs\Study\Data Analytics\Sample_Data\ManUtdPlayers.xlsx')
+# print(df)
+
+# 1. Set up multiple variables to store the titles, text within the report
+page_title_text='My report'
+title_text = 'Manchester United Players'
+text = 'Hello, This is a Report of all Man Utd Players.'
+List_Header = 'List of Man Utd Players'
+
+# 2. Combine them together using a long f-string
+html = f'''
+    <html>
+        <head>
+            <title>{page_title_text}</title>
+        </head>
+        <body>
+            <h1>{title_text}</h1>
+            <p>{text}</p>
+            <h2>{List_Header}</h2>
+            {df.to_html()}
+        </body>
+    </html>
+    '''
+# 3. Write the html string as an HTML file
+with open('html_report01.html', 'w') as f:
+    f.write(html)
